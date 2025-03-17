05cddb9bdd08299fd4aeaa9f4663dbf1ec375b08
brandonchen <zipengchen1832@gmail.com>
2025-03-17 14:20:41 -0400

    lifecycle methods

--- advance/src/App.tsx
+++ advance/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import ClassComponentsDemo from "./class";
 // when importing from a folder, it will look for the index file
 // when importing a "default export", you can name it whatever you want
 import DescribingTheUI from "./DescribingTheUI";
@@ -6,10 +7,10 @@ import DescribingTheUI from "./DescribingTheUI";
 function App() {
   return (
     <div>
-      <DescribingTheUI />
+      {/* <DescribingTheUI /> */}
+      <ClassComponentsDemo />
     </div>
   );
 }
-
 
 export default App;
