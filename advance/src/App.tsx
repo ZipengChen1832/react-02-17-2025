8948ba654df4822c1da200282a1599726b62c43f
brandonchen <zipengchen1832@gmail.com>
2025-03-20 13:57:13 -0400

    controlled component and form

--- advance/src/App.tsx
+++ advance/src/App.tsx
@@ -4,13 +4,15 @@ import ClassComponentsDemo from "./class";
 // when importing from a folder, it will look for the index file
 // when importing a "default export", you can name it whatever you want
 import DescribingTheUI from "./DescribingTheUI";
+import ManagingState from "./ManagingState";
 
 function App() {
   return (
     <div>
       {/* <DescribingTheUI /> */}
       {/* <ClassComponentsDemo /> */}
-      <AddingInteractivity />
+      {/* <AddingInteractivity /> */}
+      <ManagingState />
     </div>
   );
 }
